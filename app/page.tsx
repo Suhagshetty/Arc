77b588371e705226a420057536d43c70be7806fd
Suhagshetty <suhagshetty07@gmail.com>
2026-01-22 22:31:58 +0530

    basic nextjs and dark mode setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
+
 export default function Arc(){
+  const {setTheme} = useTheme()
   return(
     <div>
       <h2 className="text-3xl font-extrabold text-red-400 text-center">Welcome to Arc</h2>
+      <Button>Hello</Button>
+      <Button onClick={()=>setTheme("light")}>Light Mode</Button>
+      <Button onClick={()=>setTheme("dark")}>Dark Mode</Button>
     </div>
   )
 }
